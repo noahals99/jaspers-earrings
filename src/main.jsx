38636ac8f525ae15bcc3f45972c83d6f9113a8f4
[noahals99] <noah.alsina2@gmail.com>
2023-11-29 12:05:31 -0800

    Changed the root route in the main.jsx file to the correct name

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, } from "react-router-dom"
-import Root from './routes/root';
+import Root from './routes/Root';
 import ErrorPage from './routes/ErrorPage'
 import HomePage from './routes/HomePage';
 import LoginPage from './routes/LoginPage';
